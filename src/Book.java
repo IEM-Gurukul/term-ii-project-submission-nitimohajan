307434f15a70ce1312331ec24adc342ba5ce03be
nitimohajan <nitimohajan125@gmail.com>
2026-03-20 20:47:20 +0530

    apply encapsulation to Book class with private fields and getters

--- src/Book.java
+++ src/Book.java
@@ -1,13 +1,25 @@
 public class Book {
-    int id;
-    String title;
-    String author;
-    boolean isAvailable = true;
+    private int id;
+    private String title;
+    private String author;
+    private boolean isAvailable = true;
 
     public Book(int id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
     }
 
     public void borrowBook() {
