eb165b93ed6d2963eda98c247016213a936ed638
nitimohajan <nitimohajan125@gmail.com>
2026-03-20 18:45:59 +0530

    add Book class basic structure

---
+++ src/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    int id;
+    String title;
+    String author;
+    boolean isAvailable = true;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+}
+
